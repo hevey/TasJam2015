03707465a8d09ba24e5bbda749067beb38484c46
Shaun Hevey <shaun@hevey.co>
2015-09-12 16:52:20 +1000

    Updated Camera and created player prefab

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	public float height = 10f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Camera main = Camera.main;
+		var newVector = new Vector3 (transform.position.x, transform.position.y, transform.position.z - 10);
+		main.transform.position = newVector;
+		
+	}
+}

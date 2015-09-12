92d6c48f0c5f1ce4152866a402de211e6a26715a
Shaun Hevey <shaun@hevey.co>
2015-09-12 20:32:02 +1000

    Created Wall and Level Generation and added test level

--- Assets/Scripts/Level/LevelCreation.cs
+++ Assets/Scripts/Level/LevelCreation.cs
@@ -7,38 +7,16 @@ public class LevelCreation : MonoBehaviour
 {
 
     public Camera Camera;
-
+	public Color WallColour = Color.white;
+	public Color FloorColour = Color.clear;
     private WallCreation wc = new WallCreation();
 
     // Use this for initialization
     void Start()
     {
         Camera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
-        //var texture = wc.createEdgeTop(Color.green, Color.clear);
-        var texture = wc.createEdgeBottom(Color.green, Color.clear);
-
-        texture.Apply();
-        Sprite spr = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(1f, 1f), 40);
-        GameObject sprGameObj = new GameObject();
-        for (int iy = 0; iy < 100; iy++)
-        {
-            for (int ix = 0; ix < 100; ix++)
-            {
-
-                sprGameObj.name = "Floor";
-                sprGameObj.AddComponent<SpriteRenderer>();
-                SpriteRenderer sprRenderer = sprGameObj.GetComponent<SpriteRenderer>();
-                sprRenderer.sprite = spr;
-
-                Vector3 temp = new Vector3((ix * sprGameObj.GetComponent<Renderer>().bounds.size.x), iy * sprGameObj.GetComponent<Renderer>().bounds.size.y, 0);
-                Instantiate(sprGameObj);
-                sprGameObj.transform.position = temp;
-      
-            }
-        }
-        
-
-       /* TextAsset asset = (TextAsset)Resources.Load("test");
+		var textures = createTextures ();
+       	TextAsset asset = (TextAsset)Resources.Load("test");
         List<GameObject> block = new List<GameObject>();
 
         if (asset != null)
@@ -54,44 +32,52 @@ public class LevelCreation : MonoBehaviour
                 {
                     if (rowitem.Name == "row")
                     {
-                        UnityEngine.GameObject[] str = rowitem.InnerText.Split(',');
+                        var str = rowitem.InnerText.Split(',');
+						var rowNum = Int32.Parse(rowitem.Attributes["num"].Value);
+						var current = -5;
                         foreach (var a in str)
                         {
-                            block.Add(a);
+							int aa = Int32.Parse(a);
+							Sprite spr = Sprite.Create(textures[aa], new Rect(0, 0, textures[aa].width, textures[aa].height), new Vector2(1f, 1f), 40);
+							GameObject sprGameObj = new GameObject();
+							sprGameObj.name = "Floor";
+							sprGameObj.AddComponent<SpriteRenderer>();
+							SpriteRenderer sprRenderer = sprGameObj.GetComponent<SpriteRenderer>();
+							sprRenderer.sprite = spr;
+
+							Vector3 temp = new Vector3((current * sprGameObj.GetComponent<Renderer>().bounds.size.x), rowNum-4 * sprGameObj.GetComponent<Renderer>().bounds.size.y, 0);
+							sprGameObj.transform.position = temp;
+							current++;
                         }
                     }
                 }
             }
-        }*/
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+	Texture2D[] createTextures ()
     {
+		Texture2D[] texs = new Texture2D[19];
+		texs [0] = wc.createBlank (WallColour, FloorColour);
+		texs [1] = wc.createEdgeTop (WallColour, FloorColour);
+		texs [2] = wc.createEdgeBottom (WallColour, FloorColour);
+		texs [3] = wc.createEdgeLeft (WallColour, FloorColour);
+		texs [4] = wc.createEdgeRight (WallColour, FloorColour);
+		texs [5] = wc.createEdgeRightBottom (WallColour, FloorColour);
+		texs [6] = wc.createEdgeLeftBottom (WallColour, FloorColour);
+		texs [7] = wc.createEdgeLeftTop (WallColour, FloorColour);
+		texs [8] = wc.createEdgeRightTop (WallColour, FloorColour);
+		texs [9] = wc.createEdgeTopRightBottom (WallColour, FloorColour);
+		texs [10] = wc.createEdgeRightBottomLeft (WallColour, FloorColour);
+		texs [11] = wc.createEdgeBottomLeftTop (WallColour, FloorColour);
+		texs [12] = wc.createEdgeLeftTopRight (WallColour, FloorColour);
+		texs [13] = wc.createEdgeRightBottomInternal (WallColour, FloorColour);
+		texs [14] = wc.createEdgeLeftBottomInternal (WallColour, FloorColour);
+		texs [15] = wc.createEdgeRightTopInternal (WallColour, FloorColour);
+		texs [16] = wc.createEdgeLeftTopInternal (WallColour, FloorColour);
+		texs [17] = wc.createLeftRight (WallColour, FloorColour);
+		texs [18] = wc.createTopBottom (WallColour, FloorColour);
 
-    }
-
-
-    void createTextures ()
-    {
-        // Create a new 2x2 texture ARGB32 (32 bit with alpha) and no mipmaps
-        
-        Texture2D edgeRight = new Texture2D(100, 100, TextureFormat.ARGB32, false);
-        Texture2D edgeLeft = new Texture2D(100, 100, TextureFormat.ARGB32, false);
-        Texture2D cornerTopLeft = new Texture2D(100, 100, TextureFormat.ARGB32, false);
-        Texture2D cornerTopRight = new Texture2D(100, 100, TextureFormat.ARGB32, false);
-        Texture2D cornerBottomRight = new Texture2D(100, 100, TextureFormat.ARGB32, false);
-        Texture2D cornerBottomLeft = new Texture2D(100, 100, TextureFormat.ARGB32, false);
-        Texture2D corridorHorizontal = new Texture2D(100, 100, TextureFormat.ARGB32, false);
-        Texture2D corridorVertical = new Texture2D(100, 100, TextureFormat.ARGB32, false);
-        Texture2D corridorTopRight = new Texture2D(100, 100, TextureFormat.ARGB32, false);
-        Texture2D corridorBottomRight = new Texture2D(100, 100, TextureFormat.ARGB32, false);
-        Texture2D corridorBottomLeft = new Texture2D(100, 100, TextureFormat.ARGB32, false);
-        Texture2D CorridorTopLeft = new Texture2D(100, 100, TextureFormat.ARGB32, false);
-
-
-        
-
-        // Apply all SetP;
+		return texs;
     }
 }
--- Assets/Scripts/Level/WallCreation.cs
+++ Assets/Scripts/Level/WallCreation.cs
@@ -4,10 +4,29 @@ using System.Collections.Generic;
 
 public class WallCreation {
 
+	private int blockSize = 50;
+	 
+	public Texture2D createBlank(Color wall, Color floor) {
 
-    public Texture2D createEdgeTop(Color wall, Color floor)
+		var	texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+		
+		for (int i = 0; i < texture.width; i++)
+		{
+			for (int j = 0; j < texture.height; j++)
+			{
+				texture.SetPixel(i, j, floor);
+			}
+		}
+		texture.Apply ();
+		return texture;
+	}
+    public Texture2D createEdgeBottom(Color wall, Color floor, Texture2D texture = null)
     {
-        Texture2D texture = new Texture2D(50, 50, TextureFormat.ARGB32, false);
+		bool createFloor = false;
+		if (texture == null) {
+			texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+			createFloor = true;
+		}
 
         for (int i = 0; i < texture.width; i++)
         {
@@ -17,59 +36,360 @@ public class WallCreation {
                 {
                     texture.SetPixel(i, j, wall);
                 }
-                else
+				else if(createFloor)
                 {
                     texture.SetPixel(i, j, floor);
                 }
             }
         }
-
+		texture.Apply ();
         return texture;
     }
 
-    public Texture2D createEdgeBottom(Color wall, Color floor)
+	public Texture2D createEdgeTop(Color wall, Color floor, Texture2D texture = null)
     {
-        Texture2D texture = new Texture2D(50, 50, TextureFormat.ARGB32, false);
+		bool createFloor = false;
+		if (texture == null) {
+			texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+			createFloor = true;
+		}
 
         for (int i = 0; i < texture.width; i++)
         {
-            for (int j = 0; j < texture.height - texture.height /10; j++)
+			for (int j = 0; j < texture.height; j++)
             {
-                if (j < (texture.height / 10))
+                if (j > texture.height / 10 * 9)
                 {
                     texture.SetPixel(i, j, wall);
                 }
-                else
+				else if(createFloor)
                 {
                     texture.SetPixel(i, j, floor);
                 }
             }
         }
+		texture.Apply ();
 
 
         return texture;
     }
 
-    public Texture2D createEdgeLeft(Color wall, Color floor)
+	public Texture2D createEdgeLeft(Color wall, Color floor, Texture2D texture = null)
     {
-        Texture2D texture = new Texture2D(50, 50, TextureFormat.ARGB32, false);
-
+		bool createFloor = false;
+		if (texture == null) {
+			texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+			createFloor = true;
+		}
         for (int i = 0; i < texture.width; i++)
         {
             for (int j = 0; j < texture.height; j++)
             {
-                if (j < (texture.width / 10))
+                if (i < (texture.width / 10))
                 {
                     texture.SetPixel(i, j, wall);
                 }
-                else
+				else if(createFloor)
                 {
                     texture.SetPixel(i, j, floor);
                 }
             }
         }
 
-
+		texture.Apply ();
         return texture;
     }
+
+	public Texture2D createEdgeRight(Color wall, Color floor, Texture2D texture = null)
+	{
+		bool createFloor = false;
+		if (texture == null) {
+			texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+			createFloor = true;
+		}
+		
+		for (int i = 0; i < texture.width; i++)
+		{
+			for (int j = 0; j < texture.height; j++)
+			{
+				if (i > (texture.width / 10 * 9))
+				{
+					texture.SetPixel(i, j, wall);
+				}
+				else if(createFloor)
+				{
+					texture.SetPixel(i, j, floor);
+				}
+			}
+		}
+		
+		texture.Apply ();
+		return texture;
+	}
+
+	public Texture2D createBottomLeftConnector(Color wall, Color floor, Texture2D texture = null)
+	{
+		bool createFloor = false;
+		if (texture == null) {
+			texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+			createFloor = true;
+		}
+		for (int i = 0; i < texture.width; i++)
+		{
+			for (int j = 0; j < texture.height; j++)
+			{
+				if (i < (texture.width / 10) && j < (texture.height / 10))
+				{
+					texture.SetPixel(i, j, wall);
+				}
+				else if(createFloor)
+				{
+					texture.SetPixel(i, j, floor);
+				}
+			}
+		}
+		
+		texture.Apply ();
+		return texture;
+	}
+
+	public Texture2D createTopLeftConnector(Color wall, Color floor, Texture2D texture = null)
+	{
+		bool createFloor = false;
+		if (texture == null) {
+			texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+			createFloor = true;
+		}
+		for (int i = 0; i < texture.width; i++)
+		{
+			for (int j = 0; j < texture.height; j++)
+			{
+				if (i < (texture.width / 10) && j > (texture.height / 10 * 9))
+				{
+					texture.SetPixel(i, j, wall);
+				}
+				else if(createFloor)
+				{
+					texture.SetPixel(i, j, floor);
+				}
+			}
+		}
+		
+		texture.Apply ();
+		return texture;
+	}
+
+	public Texture2D createTopRightConnector(Color wall, Color floor, Texture2D texture = null)
+	{
+		bool createFloor = false;
+		if (texture == null) {
+			texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+			createFloor = true;
+		}
+		for (int i = 0; i < texture.width; i++)
+		{
+			for (int j = 0; j < texture.height; j++)
+			{
+				if (i > (texture.width / 10 * 9) && j > (texture.height / 10 * 9))
+				{
+					texture.SetPixel(i, j, wall);
+				}
+				else if(createFloor)
+				{
+					texture.SetPixel(i, j, floor);
+				}
+			}
+		}
+		
+		texture.Apply ();
+		return texture;
+	}
+
+	public Texture2D createBottomRightConnector(Color wall, Color floor, Texture2D texture = null)
+	{
+		bool createFloor = false;
+		if (texture == null) {
+			texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+			createFloor = true;
+		}
+		for (int i = 0; i < texture.width; i++)
+		{
+			for (int j = 0; j < texture.height; j++)
+			{
+				if (i > (texture.width / 10 * 9) && j < (texture.height / 10))
+				{
+					texture.SetPixel(i, j, wall);
+				}
+				else if(createFloor)
+				{
+					texture.SetPixel(i, j, floor);
+				}
+			}
+		}
+		
+		texture.Apply ();
+		return texture;
+	}
+
+
+
+	public Texture2D createEdgeRightBottom(Color wall, Color floor)
+	{
+		Texture2D texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+		
+		texture = createEdgeRight (wall, floor);
+		texture = createEdgeBottom (wall, floor, texture);
+		
+		texture.Apply ();
+		return texture;
+	}
+
+	public Texture2D createEdgeLeftBottom(Color wall, Color floor)
+	{
+		Texture2D texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+		
+		texture = createEdgeLeft (wall, floor);
+		texture = createEdgeBottom (wall, floor, texture);
+		
+		texture.Apply ();
+		return texture;
+	}
+
+	public Texture2D createEdgeRightTop(Color wall, Color floor)
+	{
+		Texture2D texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+		
+		texture = createEdgeRight (wall, floor);
+		texture = createEdgeTop (wall, floor, texture);
+		
+		texture.Apply ();
+		return texture;
+	}
+	
+	public Texture2D createEdgeLeftTop(Color wall, Color floor)
+	{
+		Texture2D texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+		
+		texture = createEdgeLeft (wall, floor);
+		texture = createEdgeTop (wall, floor, texture);
+		
+		texture.Apply ();
+		return texture;
+	}
+
+	public Texture2D createEdgeTopRightBottom(Color wall, Color floor)
+	{
+		Texture2D texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+		
+		texture = createEdgeTop (wall, floor);
+		texture = createEdgeRight (wall, floor, texture);
+		texture = createEdgeBottom (wall, floor, texture);
+
+		
+		texture.Apply ();
+		return texture;
+	}
+
+	public Texture2D createEdgeRightBottomLeft(Color wall, Color floor)
+	{
+		Texture2D texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+		
+		texture = createEdgeRight (wall, floor);
+		texture = createEdgeBottom (wall, floor, texture);
+		texture = createEdgeLeft (wall, floor, texture);
+		
+		texture.Apply ();
+		return texture;
+	}
+
+	public Texture2D createEdgeBottomLeftTop(Color wall, Color floor)
+	{
+		Texture2D texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+		
+		texture = createEdgeBottom (wall, floor);
+		texture = createEdgeLeft (wall, floor, texture);
+		texture = createEdgeTop (wall, floor, texture);
+		
+		texture.Apply ();
+		return texture;
+	}
+
+	public Texture2D createEdgeLeftTopRight(Color wall, Color floor)
+	{
+		Texture2D texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+		
+		texture = createEdgeLeft (wall, floor);
+		texture = createEdgeTop (wall, floor, texture);
+		texture = createEdgeRight (wall, floor, texture);
+		
+		texture.Apply ();
+		return texture;
+	}
+
+	public Texture2D createEdgeRightBottomInternal(Color wall, Color floor)
+	{
+		Texture2D texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+		
+		texture = createEdgeRight (wall, floor);
+		texture = createEdgeBottom (wall, floor, texture);
+		texture = createTopLeftConnector (wall, floor, texture);
+		
+		texture.Apply ();
+		return texture;
+	}
+	
+	public Texture2D createEdgeLeftBottomInternal(Color wall, Color floor)
+	{
+		Texture2D texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+		
+		texture = createEdgeLeft (wall, floor);
+		texture = createEdgeBottom (wall, floor, texture);
+		texture = createTopRightConnector (wall, floor, texture);
+		
+		texture.Apply ();
+		return texture;
+	}
+	
+	public Texture2D createEdgeRightTopInternal(Color wall, Color floor)
+	{
+		Texture2D texture = new Texture2D(blockSize, blockSize, TextureFormat.ARGB32, false);
+		
+		texture = createEdgeRight (wall, floor);
+		texture = createEdgeTop (wall, floor, texture);
+		texture = createBottomLeftConnector (wall, floor, texture);
+		texture.Apply ();
+		return texture;
+	}
+	
+	public Texture2D createEdgeLeftTopInternal(Color wall, Color floor)
+	{
+		Texture2D texture = new Texture2D (blockSize, blockSize, TextureFormat.ARGB32, false);
+		
+		texture = createEdgeLeft (wall, floor);
+		texture = createEdgeTop (wall, floor, texture);
+		texture = createBottomRightConnector (wall, floor, texture);
+		
+		texture.Apply ();
+		return texture;
+	}
+
+	public Texture2D createLeftRight(Color wall, Color floor)
+	{
+		Texture2D texture = new Texture2D (blockSize, blockSize, TextureFormat.ARGB32, false);
+		texture = createEdgeLeft (wall, floor);
+		texture = createEdgeRight (wall, floor, texture);
+		
+		texture.Apply ();
+		return texture;
+	}
+
+	public Texture2D createTopBottom(Color wall, Color floor)
+	{
+		Texture2D texture = new Texture2D (blockSize, blockSize, TextureFormat.ARGB32, false);
+		texture = createEdgeTop (wall, floor);
+		texture = createEdgeBottom (wall, floor, texture);
+		
+		texture.Apply ();
+		return texture;
+	}
 }

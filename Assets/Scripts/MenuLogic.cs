e7bb43b7ede42ca42f9ccea0d516588a96097b1b
Glenn Hevey <glenn.hevey@gmail.com>
2015-09-13 15:13:56 +1000

    Updated Menu

---
+++ Assets/Scripts/MenuLogic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuLogic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        var touchCount = Input.touchCount;
+        if (touchCount > 0)
+	    {
+            print("Got Here");
+	        Application.LoadLevel("Level1");
+	    }
+	}
+}

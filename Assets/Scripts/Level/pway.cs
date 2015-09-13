086c1c05b259eb9f6242737bc50071c56c6f8af3
Shaun Hevey <shaun@hevey.co>
2015-09-13 17:04:21 +1000

    added way

---
+++ Assets/Scripts/Level/pway.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pway : MonoBehaviour {
+
+	public string type;
+	public string audio;
+	public string row;
+	public string pos;
+
+
+	public void playAudio() {
+		switch (audio) {
+		default:
+			break;
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

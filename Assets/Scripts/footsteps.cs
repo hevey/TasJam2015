22d6317e8a893908b9c1f381cf3929ea7e3c1b16
may <maywang529@gmail.com>
2015-09-13 17:01:52 +1000

    added footstep audio source

--- Assets/Scripts/footsteps.cs
+++ Assets/Scripts/footsteps.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class footsteps : MonoBehaviour {
-
+	//http://www.soundjay.com/footsteps-1.html  footstep audio source
     public void play()
     {
 		AudioSource audio = GameObject.FindGameObjectWithTag ("footsteps").GetComponent<AudioSource> ();
